e2f3127ba4f39d5145be8d034611b1e76c0b0149
Angelines Yaport-Garcia <angelinesyg@gmail.com>
2021-06-04 13:24:34 -0400

    typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 //const session = require('express-session')
 
 
